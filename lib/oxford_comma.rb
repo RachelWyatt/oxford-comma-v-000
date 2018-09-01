9c8b53800b76b2bfbcba68b02f3e14c254ec59f3
Rachel Wyatt <rw126@evansville.edu>
2018-09-01 19:43:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,13 @@ def oxford_comma(array)
 if array.size <=2
   array.join(" and ")
 elsif array.size >=3
-  array.insert(-2, 'and')
+  
   #array.join (", ")
   #array.delete_at(-3)
   array.each do |element|
     array.insert ", "
   end
+  array.insert(-2, 'and')
   array.join
   return array
 end
